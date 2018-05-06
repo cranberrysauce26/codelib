53e2f6fea83d18d81c49e19657d4474abe869d53
Steven <stevenmai132@yahoo.ca>
2018-05-06 09:35:56 -0400

    ricehub clarification

--- greedy/ricehub.cpp
+++ greedy/ricehub.cpp
@@ -17,8 +17,9 @@ inline ll f(int l, int r) {
  * Here's how the algorithm works.
  * If you place a ricehub at some point, you will get a (possibly empty) contigious sequence of farmers.
  * e.g., all farmers from i to j. Fix i and j and suppose a placement of the ricehub takes exactly the farmers [i, j].
- * Then the sum of the distances for i and j is just X[j]-X[i] no matter where the ricehub is placed.
- * Now let's say you have farmers i+1 and j-1. Then the ricehub should be placed in between X[i+1] and X[j-1].
+ * Then obviously you place the ricehub in [X[i], X{j]] and so
+ * the sum of the distances for i and j is just X[j]-X[i] no matter where the ricehub is placed in this interval.
+ * Now let's say you have farmers i+1 and j-1. Then the ricehub should be placed in between X[i+1] and X[j-1], and so on for i+1, j-2, etc.
  * This is the greedy step. If you continue this way, you get the difference between the sum of the upper half and the 
  * sum of the lower half. (The exact function is f above, where sum is 1 indexed).
  * 
