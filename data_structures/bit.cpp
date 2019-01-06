33aac0dcfa28a12aaeee278cbeb20fc92df5abca
smhx <stevenmai62@gmail.com>
2019-01-06 12:37:38 -0500

    new binary indexed tree implementation

--- data_structures/bit.cpp
+++ data_structures/bit.cpp
@@ -1,117 +1,89 @@
-#include <algorithm>
-#include <cassert>
-#include <cstdio>
-const int MAXN = 1e5 + 5;
+#include <bits/stdc++.h>
+using namespace std;
 
-// EVERYTHING IS 1-INDEXED!!
+// bit maintains a prefix sum for the array a[1...n]
+template <typename T>
+struct bit {
+    vector<T> b;
+    int lg, n;
 
-namespace point_update_range_query {
-namespace prefix {
-int bit[MAXN];
+    bit(int m) {
+        n = m;
+        b = vector<T>(n + 1);
+        lg = 0;
+        while ((1 << lg) <= n) ++lg;
+    }
 
-// add v to value at x.
-void update(int x, int v) {
-    for (; x < MAXN; x += x & -x)
-        bit[x] += v;
-}
+    // Increments index i by value
+    void add(int i, T value) {
+        assert(i > 0 && i <= n);
+        for (; i < b.size(); i += i & -i) b[i] += value;
+    }
 
-// sum from [1, x]
-int query(int x) {
-    int res = 0;
-    for (; x; x -= x & -x)
-        res += bit[x];
-    return res;
-}
-} // namespace prefix
+    // Returns the sum of [1, i]
+    T sum(int i) const {
+        assert(i >= 0 && i <= n);
+        T ret = 0;
+        for (; i; i -= i & -i) ret += b[i];
+        return ret;
+    }
 
-namespace suffix {
-int bit[MAXN];
+    // Status: Untested
+    // Returns the i'th entry. Equivalent to sum(i)-sum(i-1)
+    T entry(int i) const {
+        assert(i > 0 && i <= n);
+        T ret = 0;
+        int z = i - (i & -i);
+        for (--i; i != z; i -= i & -i) ret += b[i];
+        return ret;
+    }
 
-// add v to value at x
-void update(int x, int v) {
-    for (; x; x -= x & -x)
-        bit[x] += v;
-}
+    // The following functions work only if all array entries are non-negative
 
-// sum from [x, MAXN-1]
-int query(int x) {
-    int res = 0;
-    for (; x < MAXN; x += x & -x)
-        res += bit[x];
-    return res;
-}
-} // namespace suffix
-} // namespace point_update_range_query
+    // Status: Tested
+    // Returns the smallest j > i such that sum(j) - sum(i) > value, or end() if no such j exists
+    // Equivalently the smallest j such that sum(j) > value + sum(i)
+    int upperbound(int i, T value) const {
+        assert(value >= 0 && i <= n && i >= 0);
+        value += sum(i);
+        i = 0;
+        for (int k = lg; k >= 0; --k) {
+            if (i + (1 << k) <= n && b[i + (1 << k)] <= value) {
+                i += 1 << k;
+                value -= b[i];
+            }
+        }
+        return i + 1;
+    }
 
-namespace range_update_point_query {
-int bit[MAXN];
+    // Status: Untested
+    // Returns the smallest j > i such that sum(j) - sum(i) >= value or end() if no such j exists
+    int lowerbound(int i, T value) const {
+        assert(value > 0 && i <= n && i >= 0);
+        value += sum(i);
+        i = 0;
+        for (int k = lg; k >= 0; --k) {
+            if (i + (1 << k) <= n && b[i + (1 << k)] < value) {
+                i += 1 << k;
+                value -= b[i + (1 << k)];
+            }
+        }
+        return i + 1;
+    }
 
-// add v to value at x.
-void update(int x, int v) {
-    for (; x; x -= x & -x)
-        bit[x] += v;
-}
+    // Status: Tested
+    // If you think of the bit as a set, this is the next (nonzero) element
+    int next(int i) const {
+        return upperbound(i, 0);
+    }
 
-// sum from [1, x]
-int query(int x) {
-    int res = 0;
-    for (; x < MAXN; x += x & -x)
-        res += bit[x];
-    return res;
-}
-} // namespace range_update_point_query
+    // TODO: Implement prev
+    int prev(int i) const {
+        return -1;
+    }
 
-namespace range_update_range_query {
-int bit1[MAXN], bit2[MAXN];
-
-void internal_update(int x, int v, int *bit) {
-    for (; x < MAXN; x += x & -x)
-        bit[x] += v;
-}
-
-int internal_query(int x, int *bit) {
-    int res = 0;
-    for (; x; x -= x & -x)
-        res += bit[x];
-    return res;
-}
-
-// adds v to all values in [l, r]
-void update(int l, int r, int v) {
-    internal_update(l, v, bit1);
-    internal_update(r + 1, -v, bit1);
-    internal_update(l, v - v * l, bit2);
-    internal_update(r + 1, v * l - v, bit2);
-}
-
-// returns the sum of elements in [1, x]
-int query(int x) {
-    return x * internal_query(x, bit1) + internal_query(x, bit2);
-}
-
-int query(int l, int r) { return query(r) - query(l - 1); }
-} // namespace range_update_range_query
-
-int bit[MAXN];
-
-// add v to value at x.
-void update(int x, int v) {
-    for (; x < MAXN; x += x & -x)
-        bit[x] += v;
-}
-
-// sum from [1, x]
-int query(int x) {
-    int res = 0;
-    for (; x; x -= x & -x)
-        res += bit[x];
-    return res;
-}
-
-int main() {
-	update(1, 1);
-	update(2, -1);
-	update(1, 1);
-	update(3, -1);
-	printf("%d\n", query(1));
-}
+    // Calls to lowerbound, next, and upperbound return end when no valid index exists
+    inline int end() const {
+        return n + 1;
+    }
+};
