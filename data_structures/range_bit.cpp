4fa0f882e6ff823a529be40d498d50a796ed00f2
smhx <stevenmai62@gmail.com>
2019-01-07 19:13:17 -0500

    generic range_bit

--- data_structures/range_bit.cpp
+++ data_structures/range_bit.cpp
@@ -1,33 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-
+template <typename T>
 struct bit {
-    vector<ll> b;
-    bit() {}
-    bit(int N) { b = vector<ll>(N + 5); }
-    ll sum(int x) {
-        ll res = 0;
+    vector<T> b;
+    bit() {
+    }
+    bit(int N) {
+        b = vector<T>(N + 5);
+    }
+
+    // Returns the sum of all elements in the range [1, x]
+    T sum(int x) {
+        T res = 0;
         for (; x; x -= x & -x) res += b[x];
         return res;
     }
-    void update(int x, ll v) {
+
+    // Adds v to the element at index x
+    void update(int x, T v) {
         for (; x < b.size(); x += x & -x) b[x] += v;
     }
 
-    void reset() { fill(b.begin(), b.end(), 0); }
+    void reset() {
+        fill(b.begin(), b.end(), 0);
+    }
 };
 
+template <typename T>
 struct range_bit {
-    bit m, b;
+    bit<T> m, b;
     range_bit(int N) {
         m = bit(N + 5);
         b = bit(N + 5);
     }
 
     // adds v to each element in [l, r]
-    void update(int l, int r, ll v) {
+    void update(int l, int r, T v) {
         m.update(l, v);
         b.update(l, -v * (l - 1));
 
@@ -36,8 +45,12 @@ struct range_bit {
     }
 
     // returns the sum of each element in [1, x]
-    ll sum(int x) { return m.sum(x) * x + b.sum(x); }
+    T sum(int x) {
+        return m.sum(x) * x + b.sum(x);
+    }
 
     // returns the sum of each element in [l, r]
-    ll sum(int l, int r) { return sum(r) - sum(l - 1); }
+    T sum(int l, int r) {
+        return sum(r) - sum(l - 1);
+    }
 };
