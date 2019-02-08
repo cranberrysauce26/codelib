c6375af865c14d308b55fd97fd9d43c856c97ce8
smhx <stevenmai62@gmail.com>
2019-02-08 12:34:37 -0500

    add ceil and floor to template

--- template.cpp
+++ template.cpp
@@ -52,6 +52,20 @@ istream& operator>>(istream& is, pair<T, V>& x) {
     return is >> x.first >> x.second;
 }
 
+template <typename T>
+T divfloor(T a, T b) {
+    assert(b != 0);
+    if (a >= 0 && b > 0 || a <= 0 && b < 0) return a / b;
+    return (a % b == 0) ? a / b : a / b - 1;
+}
+
+template <typename T>
+T divceil(T a, T b) {
+    assert(b != 0);
+    if (a >= 0 && b > 0 || a <= 0 && b < 0) return (a % b == 0) ? a / b : a / b + 1;
+    return a / b;
+}
+
 void solve();
 
 int main() {
